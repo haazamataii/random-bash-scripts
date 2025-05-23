13e6b308a043feb83edde49ae85f8f5cf5381c23
haazamataii <63615934+haazamataii@users.noreply.github.com>
2025-05-22 23:33:51 -0500

    Modified cds in ConvertToWebP.sh, modified readme, created SetVideoWallpaper.sh

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Some random bash script(s)
 
-## Converting images to WebP
+ - [Mass converting images to WebP](#converting-images-to-webp)
+ - [Video Backgrounds](#video-backgrounds)
+
+## Mass converting images to WebP
 
 Uses inodes to enter directories (max-depth) from specified directory to convert pngs, gifs, and heics to webp.
 Why to WebP (especially when heic is smaller) because thats what Jellyfin supports.
@@ -14,4 +17,46 @@ Why to WebP (especially when heic is smaller) because thats what Jellyfin suppor
 ./ConvertToWebP.sh startingDirectory
 ```
 
+### Notes:
+
 When no argument is specified, defaults to `./`
+
+### Requirements:
+
+Needs mogrify from imagemagick to work built with support for webp, png, gif, and heic
+
+## Video Backgrounds
+
+Use one or more video(s) as a background.
+
+### Usage:
+
+To clean and remove the `~/.desktopBackground/` directory, use:
+```
+./SetVideoWallpaper.sh clean
+```
+There are various options, some needed per video, and others only need to be defined once:
+```
+#multiple uses allowed
+-v <videoFile> #specify a video file
+-r <resolution> #specify a resolution (in ffmpeg's format of wxh)
+#single use
+-f <framerate> #specify the framerate in which all video(s) are to have their frames extracted at
+-s <scale> #set the scale type used for feh.
+#Valid strings in <scale> are (center, fill, max, scale)
+-t <seconds> #set the seconds between frames (a decimal since this is with sleep for ms)
+-d #disable sleep, draws frames without calling sleep in between them.
+#-d is mostly here for lower-end systems that can't draw fast enough without sleep let alone with it
+```
+
+### Notes:
+
+The multiple allowed are done per video, each video should have a corrisponding resolution.
+The single use can be done multiple times, but it overwrites the old value.
+Disable sleep is not a toggle thing so having two options to disable sleep will not turn it back on, not having it as an argument is the only way to allow sleeping.
+For scale, `--bg-` gets appended so any feh options that uses this prefix is valid for the scaling option.
+Options (the `-?` part) are not case sensitive.
+
+### Requirments:
+
+Needs ffmpeg compiled with jpg support and support for whatever video formats and encoding intended to be used. feh with jpg support is also needed to display the wallpaper.
---
+++ SetVideoWallpaper.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+###
+###### FUNCTION DEFINITIONS
+###
+
+#draw with sleep for time between frames
+drawWSleep() {
+ while true; do
+  i=0
+  feh_cmd=""
+  #build feh command with frame counters
+  while [[ $i != $videoCount ]]; do
+   feh_cmd+="\"$i\"_${currFrames[$i]}.jpg "
+   currFrames[$i]=$((${currFrames[$i]}+1))
+   #loop back to first frame (1 indexed)
+   if [[ ${currFrames[$i]} == ${maxFrames[$i]} ]]; then
+    currFrames[$i]=1
+   fi
+   i=$((i+1))
+  done
+  #have to eval for correct image path/name
+  eval "feh --bg-$scale $feh_cmd"
+  sleep $seconds
+ done
+}
+
+#draw without sleep, mostly here for weaker systems that cant draw fast enough with sleep
+#Maybe even not fast enough even without sleep
+drawWoSleep() {
+ while true; do
+  i=0
+  feh_cmd=""
+  #build feh command with frame counters
+  while [[ $i != $videoCount ]]; do
+   feh_cmd+="\"$i\"_${currFrames[$i]}.jpg "
+   currFrames[$i]=$((${currFrames[$i]}+1))
+   #loop back to first frame (1 indexed)
+   if [[ ${currFrames[$i]} == ${maxFrames[$i]} ]]; then
+    currFrames[$i]=1
+   fi
+   i=$((i+1))
+  done
+  #have to eval for correct image path/name
+  eval "feh --bg-$scale $feh_cmd"
+ done 
+}
+
+###
+#####START OF SCRIPT
+###
+
+#make backgrounds dir
+mkdir -p ~/.desktopBackground/
+home=$(pwd)
+cd ~/.desktopBackground
+rm -r * > /dev/null 2>&1
+cd "$home"
+
+#clean if user wants clean
+if [[ $1 == "clean" ]]; then
+ rmdir ~/.desktopBackground/ > /dev/null 2>&1
+ echo "Cleaned ~/.desktopBackground/ dir"
+ exit 0
+fi
+
+#check for min arg count
+if [[ -z "$8" ]]; then
+    echo "Usage: $0 -v <video_file> -f <framerate> -s <scale> -r <resolution>"
+    exit 1
+fi
+
+
+#Go through args
+state=""
+for arg in ${@}; do
+ if [[ -z "$state" ]]; then
+  #isolate state
+  state="$(echo $arg | tr '[:lower:]' '[:upper:]')"
+  state="${state:1:1}"
+  if [[ $state == "D" ]]; then
+   #no sleep
+   skipSleep="T"
+   state=""
+  fi
+ else
+  #set state vars
+  case $state in
+   "V")
+    videos+=("$arg")
+    ;;
+   "R")
+    resolutions+=("$arg")
+    ;;
+   #these 3 are set once (overwrites if another of the same option gets supplied)
+   "F")
+    framerate=("$arg")
+    ;;
+   "S")
+    scale=("$arg")
+    ;;
+   "T")
+    seconds=("$arg")
+    ;;
+   *)
+    echo "INVALID OPTION: -$state"
+    exit 2
+    ;;
+  esac
+  state=""
+ fi
+done
+
+#have ffmpeg convert frames to jpg (in self testing, the fastest image type for video background)
+i=0
+videoCount="${#videos[@]}"
+while [[ $i !=  $videoCount ]]; do
+ echo "Working on: ${videos[$i]}"
+ ffmpeg -loglevel warning -i "${videos[$i]}" -q:v 5 -vf "scale=${resolutions[$i]},fps=$framerate" ~/.desktopBackground/"$i"_%0d.jpg
+ i=$((i+1))
+done
+
+#go into backgrounds
+cd ~/.desktopBackground/
+
+#setup frame counters and get frame counts
+i=0
+while [[ $i != $videoCount ]]; do
+ frameCount=($(ls ${i}_*))
+ maxFrames+=($((${#frameCount[@]}+1)))
+ currFrames+=(1)
+ i=$((i+1))
+done
+
+#call selected draw function
+if [[ $skipSleep == "T" ]]; then
+ drawWoSleep
+else
+ drawWSleep
+fi
